0652db42a7d56a3d283359950468a0668c6149ea
yantavares <yantdo1@gmail.com>
2023-04-03 09:22:54 -0300

    12 solved

--- 1intro/12.cpp
+++ 1intro/12.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 #include <map>
+#include <string>
+
+using namespace std;
+
+string solve(string s)
+{
+    string newStr, newStr2;
+    string newChars;
+    map<char, int> scount;
+
+    for (auto const &f : s)
+        scount[f]++;
+
+    int count = 0;
+    for (auto const &b : scount)
+    {
+        if (b.second % 2 != 0)
+        {
+            count++;
+            newChars = string(b.second, b.first);
+        }
+        else
+        {
+            newStr = newStr + string(b.second / 2, b.first);
+        }
+
+        if (count > 1)
+        {
+            return "NO SOLUTION";
+        }
+    }
+
+    newStr = newStr + newChars;
+
+    for (auto const &b : scount)
+    {
+        if (b.second % 2 == 0)
+        {
+            newStr2 = string(b.second / 2, b.first) + newStr2;
+        }
+    }
+    return newStr + newStr2;
+}
 
 int main()
 {
-    int foo[]{1, 1, 1, 1, 4, 6, 4, 7, 4};
-    std::map<int, int> bar;
+    string s, end;
+    cin >> s;
+    cout << solve(s);
 
-    for (auto const &f : foo)
-        bar[f]++;
-
-    for (auto const &b : bar)
-        std::cout << "The number " << b.first << " "
-                  << "is repeated " << b.second
-                  << "times\n";
+    return 0;
 }
